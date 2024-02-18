6e6116a3dcdbcc71e806804be02ea5542ba42e58
20syl <20syl.lam78@gmail.com>
2024-02-18 12:10:57 +0100

    Regroup DOMContentLoader + Fix
    Fix catégorie 4, non ouverture au 1er clic
    Fix LocalStorage catégorie 4

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-var commandsColumnVisible1, commandsColumnVisible2, commandsColumnVisible3, commandsColumnVisible4 = true;
+var commandsColumnVisible1, commandsColumnVisible2, commandsColumnVisible3, commandsColumnVisible4 = false;
 
 function toggleCommandsColumn1() {
   var commandsColumn = document.getElementById("commands-column1");
@@ -19,17 +19,6 @@ function toggleCommandsColumn1() {
   localStorage.setItem('selectedMenu1', JSON.stringify(commandsColumnVisible1));
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu1 = localStorage.getItem('selectedMenu1');
-  if (selectedMenu1 !== null) {
-    selectedMenu1 = JSON.parse(selectedMenu1);
-    if (selectedMenu1) {
-      toggleCommandsColumn1();
-    }
-  }
-});
-
-
 function toggleCommandsColumn2() {
   var commandsColumn = document.getElementById("commands-column2");
   var toggleButton = document.querySelector(".toggle2");
@@ -48,17 +37,6 @@ function toggleCommandsColumn2() {
 
   localStorage.setItem('selectedMenu2', JSON.stringify(commandsColumnVisible2));
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu2 = localStorage.getItem('selectedMenu2');
-  if (selectedMenu2 !== null) {
-    selectedMenu2 = JSON.parse(selectedMenu2);
-    if (selectedMenu2) {
-      toggleCommandsColumn2();
-    }
-  }
-});
-
 
 function toggleCommandsColumn3() {
   var commandsColumn = document.getElementById("commands-column3");
@@ -79,17 +57,6 @@ function toggleCommandsColumn3() {
   localStorage.setItem('selectedMenu3', JSON.stringify(commandsColumnVisible3));
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu3 = localStorage.getItem('selectedMenu3');
-  if (selectedMenu3 !== null) {
-    selectedMenu3 = JSON.parse(selectedMenu3);
-    if (selectedMenu3) {
-      toggleCommandsColumn3();
-    }
-  }
-});
-
-
 function toggleCommandsColumn4() {
   var commandsColumn = document.getElementById("commands-column4");
   var toggleButton = document.querySelector(".toggle4");
@@ -108,17 +75,6 @@ function toggleCommandsColumn4() {
 
   localStorage.setItem('selectedMenu4', JSON.stringify(commandsColumnVisible4));
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedMenu4 = localStorage.getItem('selectedMenu4');
-  if (selectedMenu4 !== null) {
-    selectedMenu4 = JSON.parse(selectedMenu4);
-    if (selectedMenu4) {
-      toggleCommandsColumn4();
-    }
-  }
-});
-
 
 function switchTheme(theme) {
   var leftMenu = document.getElementById('left-menu');
@@ -141,14 +97,6 @@ function switchTheme(theme) {
 
   localStorage.setItem('selectedTheme', theme);
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  var selectedTheme = localStorage.getItem('selectedTheme');
-  if (selectedTheme) {
-    switchTheme(selectedTheme);
-  }
-});
-
 
 function copyContent(button) {
   try {
@@ -182,39 +130,99 @@ function copyContent(button) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var date = new Date();
-  var limitePromo = new Date(2024, 0, 31, 23, 59, 59);
-  var barPromo = document.getElementById('barPromo');
-  var footer = document.getElementById('footer');
-  var menu = document.getElementById('left-menu');
+function changeMessage() {
+  var messages = [
+    "🎁 <b>30%</b> de réduction sur le pack Premium jusqu'à fin Janvier. <a id='promo' href='https://shop.beacons.ai/coopbot/d66b4ff7-26ab-4af8-8967-8ca83a41a349'>Abonnez-vous maintenant</a>.",
+    "🎁 Pack Plus à <b>3,99 €</b> <span class='promoCroix'>4,99 €</span> pendant encore <span id='compteur'></span>. <a id='promo' href='https://shop.beacons.ai/coopbot/b82690a7-3782-4969-8c2e-2b48403523dc'>Abonnez-vous maintenant</a>.",
+    "🎁 Pack Standard à prix réduit, <b>-10%</b> jusqu'au 31 Janvier. <a id='promo' href='https://shop.beacons.ai/coopbot/1548ee35-dabb-4c51-a5fa-e58d7777d769'>Abonnez-vous maintenant</a>."
+  ];
 
-  if (date > limitePromo) {
-    barPromo.style.display = 'none';
-    localStorage.setItem('promoHidden', 'none');
-    footer.style.top = menu.style.top = '0px';
-  } else {
-    promoHide();
-  }
+  var randomIndex = Math.floor(Math.random() * messages.length);
+  var selectedMessage = messages[randomIndex];
 
-  setInterval(function() {
+  document.getElementById('promoText').innerHTML = selectedMessage;
+}
+
+window.onscroll = function () { updateProgressBar() };
+
+function updateProgressBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 90;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var selectedMenu1 = localStorage.getItem('selectedMenu1');
+    if (selectedMenu1 !== null) {
+        selectedMenu1 = JSON.parse(selectedMenu1);
+        if (selectedMenu1) {
+            toggleCommandsColumn1();
+        }
+    }
+
+    var selectedMenu2 = localStorage.getItem('selectedMenu2');
+    if (selectedMenu2 !== null) {
+        selectedMenu2 = JSON.parse(selectedMenu2);
+        if (selectedMenu2) {
+            toggleCommandsColumn2();
+        }
+    }
+
+    var selectedMenu3 = localStorage.getItem('selectedMenu3');
+    if (selectedMenu3 !== null) {
+        selectedMenu3 = JSON.parse(selectedMenu3);
+        if (selectedMenu3) {
+            toggleCommandsColumn3();
+        }
+    }
+
+    var selectedMenu4 = localStorage.getItem('selectedMenu4');
+    if (selectedMenu4 !== null) {
+        selectedMenu4 = JSON.parse(selectedMenu4);
+        if (selectedMenu4) {
+            toggleCommandsColumn4();
+        }
+    }
+
+    var selectedTheme = localStorage.getItem('selectedTheme');
+    if (selectedTheme) {
+        switchTheme(selectedTheme);
+    }
+
+    changeMessage();
+
     var date = new Date();
-    var tempsRestant = limitePromo - date;
-    var jours = Math.floor(tempsRestant / (1000 * 60 * 60 * 24));
-    var heures = Math.floor((tempsRestant % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((tempsRestant % (1000 * 60 * 60)) / (1000 * 60));
-    var secondes = Math.floor((tempsRestant % (1000 * 60)) / 1000);
-    document.getElementById("compteur").innerHTML = jours + "j " + heures + "h " + minutes + "m " + secondes + "s";
-  }, 1000);
+    var limitePromo = new Date(2024, 0, 31, 23, 59, 59);
+    var barPromo = document.getElementById('barPromo');
+    var footer = document.getElementById('footer');
+    var menu = document.getElementById('left-menu');
 
-  if (mainPage()) {
-    var promoState = localStorage.getItem('promoHidden');
-    barPromo.style.display = promoState || 'block';
-    var valeur = (barPromo.style.display === 'none') ? 0 : 50;
-    footer.style.top = menu.style.top = valeur + 'px';
-  }
-  
-  window.onscroll = function () { updateProgressBar() };
+    if (date > limitePromo) {
+        barPromo.style.display = 'none';
+        localStorage.setItem('promoHidden', 'none');
+        footer.style.top = menu.style.top = '0px';
+    } else {
+        promoHide();
+    }
+
+    setInterval(function() {
+        var date = new Date();
+        var tempsRestant = limitePromo - date;
+        var jours = Math.floor(tempsRestant / (1000 * 60 * 60 * 24));
+        var heures = Math.floor((tempsRestant % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((tempsRestant % (1000 * 60 * 60)) / (1000 * 60));
+        var secondes = Math.floor((tempsRestant % (1000 * 60)) / 1000);
+        document.getElementById("compteur").innerHTML = jours + "j " + heures + "h " + minutes + "m " + secondes + "s";
+    }, 1000);
+
+    if (mainPage()) {
+        var promoState = localStorage.getItem('promoHidden');
+        barPromo.style.display = promoState || 'block';
+        var valeur = (barPromo.style.display === 'none') ? 0 : 50;
+        footer.style.top = menu.style.top = valeur + 'px';
+    }
+
 });
 
 function promoHide() {
@@ -232,24 +240,4 @@ function promoHide() {
 function mainPage() {
   var url = window.location.href;
   return url.includes('/doc-coopbot/') || url.includes('/doc-coopbot/index') || url.includes('/doc-coopbot/home');
-}
-
-function changeMessage() {
-  var messages = [
-    "🎁 <b>30%</b> de réduction sur le pack Premium jusqu'à fin Janvier. <a id='promo' href='https://shop.beacons.ai/coopbot/d66b4ff7-26ab-4af8-8967-8ca83a41a349'>Abonnez-vous maintenant</a>.",
-    "🎁 Pack Plus à <b>3,99 €</b> <span class='promoCroix'>4,99 €</span> pendant encore <span id='compteur'></span>. <a id='promo' href='https://shop.beacons.ai/coopbot/b82690a7-3782-4969-8c2e-2b48403523dc'>Abonnez-vous maintenant</a>.",
-    "🎁 Pack Standard à prix réduit, <b>-10%</b> jusqu'au 31 Janvier. <a id='promo' href='https://shop.beacons.ai/coopbot/1548ee35-dabb-4c51-a5fa-e58d7777d769'>Abonnez-vous maintenant</a>."
-  ];
-
-  var randomIndex = Math.floor(Math.random() * messages.length);
-  var selectedMessage = messages[randomIndex];
-
-  document.getElementById('promoText').innerHTML = selectedMessage;
-}
-
-function updateProgressBar() {
-  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrolled = (winScroll / height) * 100;
-  document.getElementById("progress-bar").style.width = scrolled + "%";
 }
