1c2f65317325e722ba8c9075b120b52469b0d13b
20syl <20syl.lam78@gmail.com>
2024-03-13 18:59:17 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -149,7 +149,7 @@ window.onscroll = function () { updateProgressBar() };
 function updateProgressBar() {
   const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  const scrolled = (winScroll / height) * 90;
+  const scrolled = (winScroll / height) * 100;
   document.getElementById("progress-bar").style.width = scrolled + "%";
 }
 
